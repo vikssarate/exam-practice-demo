f34b819f7005e12de56ad279d65d1b0dfd7d12cf
vikssarate <viksarate01@gmail.com>
2025-08-10 15:14:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw (v1) for exam practice demo
-const CACHE = 'exam-practice-v6';
+const CACHE = 'exam-practice-v7';
 self.addEventListener('install', e => {
     self.skipWaiting();
     e.waitUntil(
